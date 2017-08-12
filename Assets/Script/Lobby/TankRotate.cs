306cf247368a5ca2cc1812e72017f048f32c15eb
pbjneo <pbjneo@naver.com>
2017-08-12 20:59:42 +0900

    lobby tank rendertexture

---
+++ Assets/Script/Lobby/TankRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TankRotate : MonoBehaviour {
+
+	float y = 5.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		y = y + 0.5f;
+		transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x, y, transform.rotation.eulerAngles.z));
+	}
+}
